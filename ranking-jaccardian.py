a6e48f8e3c60053c008a0989d898636598d71118
bharathbs93 <bbst59@mst.edu>
2018-02-14 23:18:37 -0600

    modified jaccard-rank

--- ranking-jaccardian.py
+++ ranking-jaccardian.py
@@ -20,11 +20,15 @@ jac_similarity = pairwise_distances(data_standard, metric='jaccard')
 # Reshaping array
 jac_similarity = jac_similarity.reshape(jac_similarity.size,1)
 
+print jac_similarity[0:10]
 # sorting scores
 sorted_jac = np.sort(jac_similarity, axis = None)[::-1]
 
+sorted_jac = np.roll(sorted_jac, -np.count_nonzero(np.isnan(jac_similarity)))
 #Top three paired article scores are
 top_jac = sorted_jac[0:3]
+
+print sorted_jac[0:20]
 
 print("top three jacardian score paired articles are")
 
